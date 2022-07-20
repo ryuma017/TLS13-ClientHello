18ed5ba2bd4cd8d2e0ac2b2735f001f4fe89be8c
ryuma017 <tgcrym01@gmail.com>
2022-07-20 22:15:11 +0900

    create `Payload` skeletom

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,24 @@
-use std::net::Ipv4Addr;
+#![allow(dead_code)]
+
+use std::net::{Ipv4Addr, SocketAddrV4, TcpStream};
 
 use clap::Parser;
+
+struct Payload {
+    client_version: ClientVersion,
+    client_random: ClientRandom,
+    session_id: SessionID,
+    cipher_suites: Vec<CipherSuite>,
+    compression_methods: Vec<CompressionMethod>,
+    extentions: Vec<Extension>,
+}
+
+enum ClientVersion {}
+struct ClientRandom;
+struct SessionID;
+enum CipherSuite {}
+enum CompressionMethod {}
+enum Extension {}
 
 #[derive(Parser, Debug)]
 struct Args {
@@ -8,13 +26,16 @@ struct Args {
     #[clap(short, long, value_parser, name = "ipv4-address")]
     ipv4_address: Ipv4Addr,
     #[clap(short, long, value_parser)]
-    port: u32
+    port: u16,
 }
 
+// work flow:
+// 1. connect to TLS server with TCP
+// 2. send TLS 1.3 Client Hello message
+// 3. receive decode Server Hello message
 fn main() {
     let args = Args::parse();
-    let host = args.ipv4_address;
-    let port = args.port;
 
-    println!("{host}:{port}");
+    let address = SocketAddrV4::new(args.ipv4_address, args.port);
+    let mut _stream = TcpStream::connect(address).unwrap();
 }
