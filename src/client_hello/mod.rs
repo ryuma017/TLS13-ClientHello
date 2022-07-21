9e75d5032de805a1218db3998a1c450ab54382f1
ryuma017 <tgcrym01@gmail.com>
2022-07-21 18:47:59 +0900

    split into multiple modules

---
+++ src/client_hello/mod.rs
@@ -0,0 +1,61 @@
+mod cipher_suites;
+mod client_random;
+mod client_version;
+mod compression_methods;
+mod extensions;
+mod session_id;
+
+pub use cipher_suites::*;
+pub use client_random::*;
+pub use client_version::*;
+pub use compression_methods::*;
+pub use extensions::*;
+pub use session_id::*;
+
+struct ClientHello {
+    legacy_client_version: ClientVersion,
+    client_random: ClientRandom,
+    legacy_session_id: SessionID,
+    cipher_suites: CipherSuites,
+    legacy_compression_methods: CompressionMethods,
+    extensions: Extensions,
+}
+
+impl ClientHello {
+    fn new_v1_3() -> Self {
+        todo!()
+    }
+}
+
+impl Encode for ClientHello {
+    fn encode(&self, _bytes: &mut Vec<u8>) {
+        todo!()
+    }
+}
+
+pub trait Encode {
+    fn encode(&self, bytes: &mut Vec<u8>);
+
+    fn get_encoded_bytes(&self) -> Vec<u8> {
+        let mut b = Vec::new();
+        self.encode(&mut b);
+        b
+    }
+}
+
+impl Encode for u8 {
+    fn encode(&self, bytes: &mut Vec<u8>) {
+        bytes.push(*self);
+    }
+}
+
+impl Encode for u16 {
+    fn encode(&self, bytes: &mut Vec<u8>) {
+        let be16: [u8; 2] = u16::to_be_bytes(*self);
+        bytes.extend_from_slice(&be16);
+    }
+}
+
+pub trait Legacy: Sized {
+    fn legacy() -> Self;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,9 @@
 #![allow(dead_code)]
 
-use std::net::{Ipv4Addr, SocketAddrV4, TcpStream};
+mod client_hello;
 
 use clap::Parser;
-
-struct Payload {
-    client_version: ClientVersion,
-    client_random: ClientRandom,
-    session_id: SessionID,
-    cipher_suites: Vec<CipherSuite>,
-    compression_methods: Vec<CompressionMethod>,
-    extentions: Vec<Extension>,
-}
-
-enum ClientVersion {}
-struct ClientRandom;
-struct SessionID;
-enum CipherSuite {}
-enum CompressionMethod {}
-enum Extension {}
+use std::net::{Ipv4Addr, SocketAddrV4, TcpStream};
 
 #[derive(Parser, Debug)]
 struct Args {
@@ -29,7 +14,7 @@ struct Args {
     port: u16,
 }
 
-// work flow:
+// [Workflow]
 // 1. connect to TLS server with TCP
 // 2. send TLS 1.3 Client Hello message
 // 3. receive decode Server Hello message
