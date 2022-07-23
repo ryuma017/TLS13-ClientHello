a68b103f01e04de3a12163bb8d0ff1e5355099ab
ryuma017 <tgcrym01@gmail.com>
2022-07-23 22:38:08 +0900

    add a red test for cipher suites encoding

--- src/client_hello/cipher_suites.rs
+++ src/client_hello/cipher_suites.rs
@@ -1,1 +1,20 @@
+use super::Encode;
+
 pub struct CipherSuites;
+
+#[cfg(test)]
+mod tests {
+    use crate::client_hello::Encode;
+
+    use super::CipherSuites;
+
+    #[test]
+    fn default_cipher_suites_encoding_works() {
+        let cipher_suites = CipherSuites::default();
+        let encoded = cipher_suites.get_encoded_bytes();
+
+        assert_eq!(encoded, [
+            0x00, 0x02, 0x13, 0x01
+        ]);
+    }
+}
