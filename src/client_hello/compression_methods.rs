ba33251f71e2bfc4e2143927d740841c7fa8d715
ryuma017 <tgcrym01@gmail.com>
2022-07-26 03:09:04 +0900

    add a red test for `CompressionMethods`

--- src/client_hello/compression_methods.rs
+++ src/client_hello/compression_methods.rs
@@ -1,1 +1,16 @@
 pub struct CompressionMethods;
+
+#[cfg(test)]
+mod tests {
+    use crate::client_hello::utils::{Legacy, Encode};
+
+    use super::CompressionMethods;
+
+    #[test]
+    fn compression_methods_encoding_works() {
+        let compression_methods = CompressionMethods::legacy();
+        let encoded = compression_methods.get_encoded_bytes();
+
+        assert_eq!(encoded, vec![0x01, 0x00])
+    }
+}
