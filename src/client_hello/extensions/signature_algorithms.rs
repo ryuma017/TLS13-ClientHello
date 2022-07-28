c57ec8bff1f69f6f60af08d9ce05091184a21554
ryuma017 <tgcrym01@gmail.com>
2022-07-29 01:32:37 +0900

    make a red test green (impl `SignatureAlgorithmsList`)

--- src/client_hello/extensions/signature_algorithms.rs
+++ src/client_hello/extensions/signature_algorithms.rs
@@ -2,29 +2,85 @@ use std::ops::Deref;
 
 use crate::client_hello::utils::Encode;
 
-pub struct SignatureAlgorithmList(Vec<SignatureAlgorithm>);
+pub struct SignatureAlgorithmList(Vec<SignatureScheme>);
 
 impl Encode for SignatureAlgorithmList {
     fn encode(&self, bytes: &mut Vec<u8>) {
-        todo!()
+        let mut sub: Vec<u8> = Vec::new();
+        encode_signature_schemes(&mut sub, self);
+        (sub.len() as u16).encode(bytes);
+        bytes.append(&mut sub);
     }
 }
 
 impl Default for SignatureAlgorithmList {
     fn default() -> Self {
-        todo!()
+        use SignatureScheme::*;
+
+        Self(vec![
+            ECDSA_SECP256r1_SHA256,
+            ECDSA_SECP384r1_SHA384,
+            ECDSA_SECP521r1_SHA512,
+        ])
     }
 }
 
 impl Deref for SignatureAlgorithmList {
-    type Target = Vec<SignatureAlgorithm>;
+    type Target = Vec<SignatureScheme>;
 
     fn deref(&self) -> &Self::Target {
         &self.0
     }
 }
 
-pub enum SignatureAlgorithm {}
+fn encode_signature_schemes(bytes: &mut Vec<u8>, values: &[SignatureScheme]) {
+    let mut sub: Vec<u8> = Vec::new();
+    values.iter().for_each(|ss| {
+        ss.as_assigned_u16().encode(&mut sub);
+    });
+
+    (sub.len() as u16).encode(bytes);
+    bytes.append(&mut sub);
+}
+
+#[allow(non_camel_case_types)]
+pub enum SignatureScheme {
+    ECDSA_SECP256r1_SHA256,
+    ECDSA_SECP384r1_SHA384,
+    ECDSA_SECP521r1_SHA512,
+    ED25519,
+    ED448,
+    RSA_PSS_PSS_SHA256,
+    RSA_PSS_PSS_SHA384,
+    RSA_PSS_PSS_SHA512,
+    RSA_PSS_RSAE_SHA256,
+    RSA_PSS_RSAE_SHA384,
+    RSA_PSS_RSAE_SHA512,
+    RSA_PKCS1_SHA256,
+    RSA_PKCS1_SHA384,
+    RSA_PKCS1_SHA512,
+}
+
+impl SignatureScheme {
+    fn as_assigned_u16(&self) -> u16 {
+        match self {
+            Self::ECDSA_SECP256r1_SHA256 => 0x0403,
+            Self::ECDSA_SECP384r1_SHA384 => 0x0503,
+            Self::ECDSA_SECP521r1_SHA512 => 0x0603,
+            Self::ED25519 => 0x0807,
+            Self::ED448 => 0x0808,
+            Self::RSA_PSS_PSS_SHA256 => 0x0809,
+            Self::RSA_PSS_PSS_SHA384 => 0x080a,
+            Self::RSA_PSS_PSS_SHA512 => 0x080b,
+            Self::RSA_PSS_RSAE_SHA256 => 0x0804,
+            Self::RSA_PSS_RSAE_SHA384 => 0x0805,
+            Self::RSA_PSS_RSAE_SHA512 => 0x0806,
+            Self::RSA_PKCS1_SHA256 => 0x0401,
+            Self::RSA_PKCS1_SHA384 => 0x0501,
+            Self::RSA_PKCS1_SHA512 => 0x0601,
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -39,9 +95,7 @@ mod tests {
 
         assert_eq!(
             encoded,
-            vec![
-                0x00, 0x08, 0x00, 0x06, 0x04, 0x03, 0x05, 0x03, 0x06, 0x03
-            ]
+            vec![0x00, 0x08, 0x00, 0x06, 0x04, 0x03, 0x05, 0x03, 0x06, 0x03]
         )
     }
 }
