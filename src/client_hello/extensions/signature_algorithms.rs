d5408dca6589c5506005367ba1e46fa223720ba6
ryuma017 <tgcrym01@gmail.com>
2022-07-29 01:18:33 +0900

    add a red test for `SignatureAlgorithmsList`

--- src/client_hello/extensions/signature_algorithms.rs
+++ src/client_hello/extensions/signature_algorithms.rs
@@ -25,3 +25,23 @@ impl Deref for SignatureAlgorithmList {
 }
 
 pub enum SignatureAlgorithm {}
+
+#[cfg(test)]
+mod tests {
+    use crate::client_hello::utils::Encode;
+
+    use super::SignatureAlgorithmList;
+
+    #[test]
+    fn signature_algorithms_encoding_works() {
+        let signature_algorithms = SignatureAlgorithmList::default();
+        let encoded = signature_algorithms.get_encoded_bytes();
+
+        assert_eq!(
+            encoded,
+            vec![
+                0x00, 0x08, 0x00, 0x06, 0x04, 0x03, 0x05, 0x03, 0x06, 0x03
+            ]
+        )
+    }
+}
