3b207f2b8e4f6613dad6d6c9fae98580f33cec5c
ryuma017 <tgcrym01@gmail.com>
2022-07-28 18:01:22 +0900

    add a red test for `SupportedGroupList`

--- src/client_hello/extensions/supported_groups.rs
+++ src/client_hello/extensions/supported_groups.rs
@@ -25,3 +25,25 @@ impl Deref for SupportedGroupList {
 }
 
 pub enum SupportedGroup {}
+
+
+#[cfg(test)]
+mod tests {
+    use crate::client_hello::utils::Encode;
+
+    use super::SupportedGroupList;
+
+    #[test]
+    fn supported_groups_encoding_works() {
+        let supported_groups = SupportedGroupList::default();
+        let encoded = supported_groups.get_encoded_bytes();
+
+        assert_eq!(
+            encoded,
+            vec![
+                0x00, 0x16, 0x00, 0x14, 0x00, 0x17, 0x00, 0x18, 0x00, 0x19, 0x00, 0x1d, 0x00, 0x1e,
+                0x01, 0x00, 0x01, 0x01, 0x01, 0x02, 0x01, 0x03, 0x01, 0x04
+            ]
+        )
+    }
+}
